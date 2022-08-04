42e11cf9cf258112849d70cbd3a752c087eeca7a
gabriel montanher <gabrielmontanherr@gmail.com>
2022-08-03 23:39:23 -0300

    updates for cors

--- main.py
+++ main.py
@@ -8,8 +8,10 @@ app = FastAPI()
 
 # CORS VERIFY
 origins = [
-    "http://localhost",
+    "http://localhost:3000",
     "http://localhost:8080",
+    "http://127.0.0.1:8000",
+    "http://127.0.0.1:8000/api/users",
 ]
 app.add_middleware(
     CORSMiddleware,
